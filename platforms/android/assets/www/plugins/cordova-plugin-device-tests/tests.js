cb84a7bb173e6543131fbef603eb707df36434c9
Larry Nickerson <lnickers@us.ibm.com>
2015-08-24 08:59:39 -0500

    removing platforms folder from repo

--- platforms/android/assets/www/plugins/cordova-plugin-device-tests/tests.js
+++
@@ -1,8 +0,0 @@
-cordova.define("cordova-plugin-device-tests.tests", function(require, exports, module) { exports.defineAutoTests = function () {
-	describe('MFPCore tests', function () {
-		it('should exist', function(){
-			expect(MFPClient).toBeDefined();
-		});
-	});
-};
-});
